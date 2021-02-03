42f16ebd247a9a15a4a6661e78cdef6e06b5305f
Galaxy <galaxy78@gmail.com>
2021-02-03 11:53:40 +0200

    added kubectl,kobs

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
+1- install virtualbox
+2- Install vagrant
+3- clone the project 
+4- RUN  >>>> vagrant up
+5- RUN  >>>  vagrant ssh-config
+the command  will show you the IP,username,port and 
+Use file """"""""key file path""""""""  you can use this key to ssh to this machine
+5- Make a new ssh session and use the data provided from previous step to login to this machine
+6- To use "ubuntu" user copy the .ssh folder from vagrant home to ubuntu home and change permissions
+>>>> sudo cp -r .ssh/ /home/ubuntu/
+>>>> sudo chown ubuntu:ubuntu /home/ubuntu/
+
+
 # DevOps box
 * A vagrant project with an ubuntu box with the tools needed to do DevOps
 
@@ -5,3 +18,8 @@
 * Terraform
 * AWS CLI
 * Ansible
+* ibmcloud CLI
+* kubectl
+* KOPS
+
+
--- scripts/install.sh
+++ scripts/install.sh
@@ -42,12 +42,6 @@ pip3 install -U awsebcli
 T_VERSION=$(/usr/local/bin/terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
 T_RETVAL=${PIPESTATUS[0]}
 
-#ibmcloud
-curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
-ibmcloud plugin install container-service
-ibmcloud plugin install container-registry
-ibmcloud plugin install observe-service
-
 [[ $T_VERSION != $TERRAFORM_VERSION ]] || [[ $T_RETVAL != 0 ]] \
 && wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
 && unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
@@ -61,6 +55,24 @@ P_RETVAL=$?
 && wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
 && unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
 && rm packer_${PACKER_VERSION}_linux_amd64.zip
+
+#ibmcloud
+curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
+ibmcloud plugin install container-service
+ibmcloud plugin install container-registry
+ibmcloud plugin install observe-service
+
+#Install kubectl
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+#Install KOPS
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
+
 
 # clean up
 if [ ! ${REDHAT_BASED} ] ; then
